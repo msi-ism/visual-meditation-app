560f640da8f6ee87dc146443f23e7d6d49f13b5f
msi-ism <msism720@gmail.com>
2023-04-15 17:26:11 -0400

    active choice selector css added

--- src/VisualChooser.js
+++ src/VisualChooser.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState, useEffect, useRef} from 'react'
 import seagullPic from './images/seagull.png'
 import fishPic from './images/fish.png'
 import heartPic from './images/heartshape.png'
@@ -6,9 +7,18 @@ import './VisualChooser.css'
 
 const VisualChooser = ({setAnimation, animations}) => {
 
+    const [isActive, setIsActive] = useState(false)
+
     const handleClick = (evt) => {
         setAnimation(animations[evt.target.id])
-        console.log(evt.target.id)
+        let choices = document.querySelector('.choice-holder')
+        for (let i = 0; i < choices.children.length; i++) {
+            if (choices.children[i].classList.contains('active')) {
+                choices.children[i].classList.remove('active')
+            }
+        }
+        evt.currentTarget.classList.toggle('active')
+        // console.log(evt.target.id)
     }
 
     return (
