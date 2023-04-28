d23179dcdbe1e2b83ae3880f0703b26adf35041d
msi-ism <msism720@gmail.com>
2023-04-28 16:27:00 -0400

    ui updates

--- src/ControlButtons.js
+++ src/ControlButtons.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './ControlButtons.css'
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import start from './images/start.png'
 import nostart from './images/nostart.png'
 import stop from './images/stop.png'
@@ -48,12 +48,13 @@ const ControlButtons = ({ toggleAnimation, updateDuration, duration, durationDis
 
     return (
         <div className='control-btns'>
+            {duration !== 'false' ? <p className='duration-text'>Breaths Remaining: {durationDisplay}</p> : <p className='duration-reminder'>Please select a duration below.</p>}
             <div className='duration-row'>
                 <button className='db' id='10' onClick={updateDuration}>1min</button>
                 <button className='db' id='50' onClick={updateDuration}>5min</button>
                 <button className='db' id='100' onClick={updateDuration}>10min</button>
             </div>
-            {duration !== 'false' ? <p className='duration-text'>Breaths Remaining: {durationDisplay}</p> : <p className='duration-reminder'>Please select a duration above.</p>}
+
             {duration !== 'false' ? <div className="play-btn" onClick={toggleAnimation}><img className='play-img' src={playButton}></img></div> : <div className="play-btn" onClick={durationReminder}><img className='play-img' src={nostart}></img></div>}
         </div>
     );
